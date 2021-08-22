c3ed8b59250e1a2495c890c1dbb09a4c0c26f506
dlghrms1992 <dlghrms1992@naver.com>
2021-08-22 21:20:01 +0900

    URL을 통해서 입력된 값 사용하기

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,19 +1,22 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 
 });
 app.listen(3000);
 // http : protocol -> 웹브라우저와 웹서버가 데이를 주고받기위한 통신규칙
 // domainName or hostName -> ip 주소 특정한 컴퓨터
 // port : 어떤 port에 연결되어 있는 서버 통신
+// ?xxx&xxxx =>queryStirng
